a4ee96578c614fb14f7c03cb364ee968afc595d0
sharkhouse23 <ocwear@gmail.com>
2018-11-10 14:53:11 -0800

    Refactored proxy server incorporate AWS served components

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-require('../client/header-sidebar-service/server/index.js');
-require('../client/instructors-service/server/index.js');
-require('../client/Student-Feedback/server/index.js');
-require('../client/students-also-bought-service/server/server.js');
+// require('../client/header-sidebar-service/server/index.js');
+// require('../client/instructors-service/server/index.js');
+// require('../client/Student-Feedback/server/index.js');
+// require('../client/students-also-bought-service/server/server.js');
 
 const express = require('express');
 const proxy = require('express-http-proxy');
@@ -14,10 +14,10 @@ const app = express();
 app.use('/courses/:courseId', express.static(path.join(__dirname, '/../public')));
 
 
-app.get('/courses/:courseId/header', proxy('http://localhost:3003/'));
-app.get('/:Id/instructors', proxy('http://localhost:3002/'));
-app.get('/courses/:courseId/similarcourses', proxy('http://localhost:3004/'));
-app.get('/:courseId/reviews', proxy('http://localhost:3001/'));
+app.get('/courses/:courseId/header', proxy('http://ec2-54-219-155-194.us-west-1.compute.amazonaws.com/'));
+app.get('/:Id/instructors', proxy('http://udemo-env.bhd58pxyfd.us-east-2.elasticbeanstalk.com/'));
+app.get('/courses/:courseId/similarcourses', proxy('http://ec2-13-57-196-203.us-west-1.compute.amazonaws.com/'));
+app.get('/:courseId/reviews', proxy('http://ec2-18-225-7-76.us-east-2.compute.amazonaws.com/'));
 
 app.listen(port, () => {
     console.log(`server running at http://localhost:${port}`);
